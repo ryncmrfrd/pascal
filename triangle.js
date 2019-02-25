8ef94fd1f7f79ef6d8699b28c294ae30dc1fc460
Ryan Comerford <40415962+ryncmrfrd@users.noreply.github.com>
2019-02-25 18:01:50 +1100

    Better footer

--- triangle.js
+++ triangle.js
@@ -1,11 +1,15 @@
 var triangle = [], lastrow = [];
 function calculate(n){
     $('.results').empty().removeClass('scroll');
-    if(n < 1000 && n != '' && Math.sign(n) == 1 && n > 2){
+    if(n < 1000 && n != '' && n > 2){
         if(n > 16){$('.results').addClass('scroll');}
         CalcTriangle(n);
     }
-    else{$('.results').append('<span class="label error">You did it wrong.</span>');}
+    else{
+        if(n == ''){$('.results').append('<span class="label error">Please enter a number.</span>');}
+        else if(n < 3){$('.results').append('<span class="label error">Sorry, the number has to be equal to or higher than 3.</span>');}
+        if(n >= 1000){$('.results').append('<span class="label error">Please enter a number lower than 1000.</span>');}
+    }
 }
 function CalcTriangle(n){
     lastrow = [1,1];
