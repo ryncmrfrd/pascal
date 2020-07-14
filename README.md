64707d7460c01b061bce1af121f64edc04c06230
Ryan Comerford <ryncmrfrd@outlook.com>
2020-07-14 12:33:47 +1000

    New website (terminal)

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
-# Pascal's Triangle Generator
-An almost completely useless welsite for creating funky maths shapes.
-Well, one shape in particular - the Pascal's Triangle.
-If you don't know what I'm on about, [Wikipedia](https://en.wikipedia.org/wiki/Pascal's_triangle) will tell you, or you can just look the below slick animation:
-
+# Pascal's Triangle Calculator
+An almost completely useless [website](https://pascal.ryncmrfrd.com/) which calculates pascal's triangle.
+If you're curious, take a peak at [Wikipedia](https://en.wikipedia.org/wiki/Pascal's_triangle) or have a look at this slick animation:
 ![Pascal's Triangle Explanation](https://upload.wikimedia.org/wikipedia/commons/0/0d/PascalTriangleAnimated2.gif "Pascal's Triangle Explanation")
-
-### What now?
-To generate your own cool triangle, download [pascal.min.js](https://github.com/ryncmrfrd/pascal/blob/master/pascal.min.js) and run: ` pascal(number) `
-This will return a promise, so you can do things like:` pascal(10).then(data => console.log(data) ) `\
-
-Or alternatively, you can read the code explanation and have a look at the unminified version at [pascal.js](https://github.com/ryncmrfrd/pascal/blob/master/pascal.js).
 
 ### Build with :coffee: by [Ryan Comerford](https://ryncmrfrd.com) 
--- pascal.js
+++ pascal.js
@@ -1,33 +1,14 @@
 /**
-    CALCULATING PASCAL'S TRIANGLE IN JS
-    @author Ryan Comerford <https://ryncmrfrd.com>\
-    For minified/production code @see pascal.min.js :)
+    CALCULATING A PASCAL'S TRIANGLE IN JS
+    @author Ryan Comerford <https://ryncmrfrd.com>
 */
 
-/** @function pascal - define async function (allowing for .then() usage like a promise) */
-const pascal = async function(number){
-
-    var results = [[1]], /** @var results - all currently canculated rows to return at the end */
-        latestrow = [1]; /** @var latestrow - the last calculated row */
-
-    /** @for the number of rows to be calculated */
-    for(var i = 0; i < number - 1; i++){
-
-        var nextrow = []; /** @var nextrow - each integers calculated in the row */
-        
-        /** @for the number of integers in the current row */
-        for(var y = 0; y < (latestrow.length + 1); y++){
-
-            /** add the next integer to the nextrow @see nextrow */
-            nextrow.push((latestrow[y - 1] || 0) + (latestrow[y] || 0))
-        }
-
-        /** set lastrow to the newest "latest calculated row" @see latestrow */
-        latestrow = nextrow;
-
-        /** add the new to results @see results */
-        results.push(nextrow);
+p=n=>{
+    r=[[1]];
+    for(i=n-1;i--;){
+        c=[];
+        [0,...[...r].pop()].forEach((t,i,a)=>c.push(t+(a[i+1]||0)));
+        r.push(c)
     }
-
-    return results;
+    return r
 }
